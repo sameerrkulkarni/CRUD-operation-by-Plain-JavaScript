40fb25cf1a47f4293e086540bbf34262aa014947
Sameer Kulkarni <97330143+sameerrkulkarni@users.noreply.github.com>
2022-09-25 15:45:47 +0530

    Update fetch1.js

--- fetch1.js
+++ fetch1.js
@@ -1,107 +1,77 @@
-const postList = document.querySelector('.post-list');
-const url = 'http://localhost:3002/user';
-const post = document.querySelector('.addPost');
-const title = document.getElementById('title');
-const content = document.getElementById('content');
-const add=document.querySelector('.Adding')
-let output = '';
+const post1=document.querySelector('.post-list');
+const sname=document.getElementById('title');
+const email=document.getElementById('content');
+const add=document.querySelector('.addPost');
+// output='';
 
-const renderposts = (posts)=>{
-posts.forEach(post=>{
-    output += `
-    <div class="post-list">
-        <h5>${post.name}</h5>
-        <p>${post.email}</p>
-        <button>Edit</button>
-        <button>delete</button>
-    </div>
-`;
+fetch('http://localhost:3002/user')
+.then(res=>res.json())
+.then(data=>
+    data.forEach(post=>{
+        post1.innerHTML +=
+        `<div data-id=${post.id}>
+        <h3 class='name'>${post.name}</h3>
+        <h5 class='email'>${post.email}</h5>
+        <button id="edit">EdiT</button>
+        <button id="delete">DeletE</button>
+        </div>`        
+}))
+
+add.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    fetch('http://localhost:3002/user',{
+    method:'POST',
+    headers:{'Content-type':'application/json'},
+    body:JSON.stringify({
+        name:sname.value,
+        email:email.value
+    })
 })
-}
-// GET (Read Posts)
-
-fetch(url)
 .then(res=>res.json())
 .then(data=>{
-data.forEach(post =>{
-    output += `
-    <div class="post-list">
-        <div data-id=${post.id}>
-        <h5 class="pname">${post.name}</h5>
-        <p class="pemail">${post.email}</p>
-        <button id="edit">Edit</button>
-        <button id="delete">delete</button>
-        </div>
-    </div>
-`;
+    const dataArray=[];
+    dataArray.push(data);
 })
-postList.innerHTML=output  
+.then(()=>{location.reload()})
+})
 
-postList.addEventListener('click',(e)=>{
+post1.addEventListener('click',(e)=>{
     e.preventDefault();
-    let deletePress =e.target.id=='delete';
-    let editPress =e.target.id=='edit';
-    let id = e.target.parentElement.dataset.id;
+    let deleteClick = e.target.id=='delete';
+    let editClick = e.target.id=='edit';
+    let id=e.target.parentElement.dataset.id;
 
-    // DLETE
-    if(deletePress){
-        fetch(`${url}/${id}`,{
-            method:'DELETE',
+    //DELETE
+    if(deleteClick){
+        const doConfirm = confirm('do u want to delete.!');
+        if(doConfirm){fetch(`http://localhost:3002/user/${id}`,{
+        method:'DELETE'
         })
         .then(res=>res.json())
-        .then(()=>location.reload())
+        .then(()=>location.reload())}
     }
 
-    if(editPress){
-    const parent = e.target.parentElement;
-    let titleN=parent.querySelector('.pname').textContent;
-    let titleE=parent.querySelector('.pemail').textContent;
-    // console.log(titleN,titleE);
-    title.value=titleN;
-    content.value=titleE;
-    }
-    // UPDATE existing post
-    // Method FETCH
-    add.addEventListener('click',(e)=>{
-        e.preventDefault();
-        // console.log('post');
-        fetch(`${url}/${id}`,{
-            method:'PATCH',
-            headers:{'Content-Type':'application/json'},
-            body:JSON.stringify({
-                name:title.value,
-                email:content.value
+    //EDIT
+    if(editClick){
+        let parent=e.target.parentElement;
+        let nameN = parent.querySelector('.name').textContent;
+        let emailN = parent.querySelector('.email').textContent;
+        // console.log(emailN);
+        title.value=nameN;
+        content.value=emailN;
+
+        add.addEventListener('click',(e)=>{
+            e.preventDefault();
+            fetch(`http://localhost:3002/user/${id}`,{
+                method:'PATCH',
+                headers:{'Content-type':'application/json'},
+                body:JSON.stringify({
+                    name:title.value,
+                    email:content.value,
+                })
             })
+            .then(res=>res.json())
+            // .then(()=>location.reload())
         })
-        .then(res=>res.json())
-        .then(()=>location.reload())
-    })
+    }
 })
-});
-
-// CREATE - insert new post
-// method POST
-post.addEventListener('submit',(e)=>{
-e.preventDefault();
-console.log(title.value);
-console.log(content.value);
-fetch(url,
-    {
-        method:'POST',
-        headers:{
-            'Content-type':'application/json'
-        },
-        body: JSON.stringify({
-            name:title.value,
-            email:content.value
-        })
-    })
-    .then(res=>res.json())
-    .then(data=>{
-        const dataAray=[];
-        dataAray.push(data);
-        renderposts(dataAray);
-    })
-})
-
- 
